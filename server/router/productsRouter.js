ff04c378a247c338dae3e22b9f9b735f66903535
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-09-22 15:50:29 -0300

    corrigido a resposta enviada pela rota /productStar

--- server/router/productsRouter.js
+++ server/router/productsRouter.js
@@ -26,7 +26,34 @@ router.get("/productStar/:product_id", async function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   let productId = req.params.product_id;
   let products = await productsService.getProductStar(productId);
-  res.json(products);
+  let arrayOfStars = [];
+  let totalStars = 0;
+  let numberOfStars = 1;
+  let ratingStar = 0;
+
+  arrayOfStars[0] = await products[0].one_star;
+  arrayOfStars[1] = await products[0].two_star;
+  arrayOfStars[2] = await products[0].three_star;
+  arrayOfStars[3] = await products[0].four_star;
+  arrayOfStars[4] = await products[0].five_star;
+  totalStars = await (products[0].one_star +
+    products[0].two_star +
+    products[0].three_star +
+    products[0].four_star +
+    products[0].five_star);
+
+  for (let i = 0; i < 5; i++) {
+    let aux = ratingStar;
+    ratingStar = await arrayOfStars[i];
+    if (aux > ratingStar) {
+      ratingStar = aux;
+    }
+  }
+  numberOfStars = arrayOfStars.lastIndexOf(ratingStar) + 1;
+
+  let resp = [{ totalStars, numberOfStars }];
+
+  res.json(resp);
 });
 
 module.exports = router;
