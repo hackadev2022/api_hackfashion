ace85efb4cf7a173a1bb0a55d494d3442a795c99
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-09-25 11:05:59 -0300

    tirei a lógica do router e passei para o service

--- server/router/customersRouter.js
+++ server/router/customersRouter.js
@@ -6,36 +6,9 @@ router.post("/login", async function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   let loginEmail = req.body.loginEmail;
   let loginPassword = req.body.loginPassword;
-  let customer = await customersService.postLogin(loginEmail);
+  let customer = await customersService.postLogin(loginEmail, loginPassword);
 
-  if (customer[0] === undefined) {
-    res.json([
-      {
-        customer_id: "",
-        name: "",
-        phone: "",
-        loged: "notFound",
-      },
-    ]);
-  } else if (customer[0].password != loginPassword) {
-    res.json([
-      {
-        customer_id: "",
-        name: "",
-        phone: "",
-        loged: "wrongPassword",
-      },
-    ]);
-  } else if (customer[0].password === loginPassword) {
-    res.json([
-      {
-        customer_id: customer[0].customer_id,
-        name: customer[0].name,
-        phone: customer[0].phone,
-        loged: true,
-      },
-    ]);
-  }
+  res.json(customer);
 });
 
 router.post("/custumer", async function (req, res) {
--- server/service/customersService.js
+++ server/service/customersService.js
@@ -1,7 +1,38 @@
 const customersData = require("../data/customersData");
 
-exports.postLogin = function (customer_email) {
-  return customersData.postLogin(customer_email);
+exports.postLogin = async function (customer_email, loginPassword) {
+  let customer = await customersData.postLogin(customer_email);
+
+  if (customer[0] === undefined) {
+    customer = [
+      {
+        customer_id: "",
+        name: "",
+        phone: "",
+        loged: "notFound",
+      },
+    ];
+  } else if (customer[0].password != loginPassword) {
+    customer = [
+      {
+        customer_id: "",
+        name: "",
+        phone: "",
+        loged: "wrongPassword",
+      },
+    ];
+  } else if (customer[0].password === loginPassword) {
+    customer = [
+      {
+        customer_id: customer[0].customer_id,
+        name: customer[0].name,
+        phone: customer[0].phone,
+        loged: true,
+      },
+    ];
+  }
+
+  return customer;
 };
 
 exports.postCustomer = function (nome, email, senha) {
