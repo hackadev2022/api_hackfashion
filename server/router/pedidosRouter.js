b0c7445b4a6a5e7c0fddfa14ee71c7ba9dcac732
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-09-29 20:25:02 -0300

    removi console.logs

--- server/router/pedidosRouter.js
+++ server/router/pedidosRouter.js
@@ -8,12 +8,7 @@ router.post("/pedido", async function (req, res) {
     let customer_id = req.body.customer_id;
     let id_address = req.body.id_address;
     let total_price = req.body.total_price;
-    console.log("customer_id");
-    console.log(customer_id);
-    console.log("id_address");
-    console.log(id_address);
-    console.log("total_price");
-    console.log(total_price);
+
     let pedido_id = await pedidosService.postPedido(
       customer_id,
       id_address,
