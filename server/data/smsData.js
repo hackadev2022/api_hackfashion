69e1cf7a43776a2d2c61d40075a663b750dfe8b7
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-10-14 12:56:25 -0300

    sid e token agora no .env

--- server/data/smsData.js
+++ server/data/smsData.js
@@ -5,24 +5,21 @@ require("dotenv").config({
 const accountSid = process.env.SID;
 const authToken = process.env.AUTHTOKEN;
 
-console.log(accountSid)
-console.log(authToken)
+const client = require("twilio")(accountSid, authToken);
 
-const client = require('twilio')(accountSid, authToken);
-
-const numeroZapZap = 'whatsapp:+556293176806';
+const numeroUser = process.env.CELL_NUMBER;
+const numeroTwilio = process.env.CELL_NUMBER_TWILIO;
 
 exports.getSmsData = async function (name, pedido_id) {
-  console.log(pedido_id)
-  console.log(name)
-    
-    client.messages 
-.create({ 
-  body:  `Ola ${name} a compra foi finalizada com sucesso, Nº ${pedido_id}`, 
-  from: 'whatsapp:+14155238886',       
-  to: numeroZapZap    
-}) 
-.then(message => console.log('mensagem enviada',message.sid)) 
-.done();
-  };
+  console.log(pedido_id);
+  console.log(name);
 
+  client.messages
+    .create({
+      body: `Ola ${name} a compra foi finalizada com sucesso, Nº ${pedido_id}`,
+      from: numeroTwilio,
+      to: numeroUser,
+    })
+    .then((message) => console.log("mensagem enviada", message.sid))
+    .done();
+};
--- server/router/smsRouter.js
+++ server/router/smsRouter.js
@@ -4,17 +4,15 @@ const smsService = require("../service/smsService");
 const customersService = require("../service/customersService");
 
 router.post("/sms", async function (req, res) {
-    const pedido_id = req.body.pedido_id;
-    const customer_id = req.body.customer_id
-    try {
-        let name = await customersService.getName(customer_id)
-        await smsService.getSmsData(name[0].name, pedido_id)
-        res.send('sucess')        
-    } catch (error) {
-        res.send(error)
-    }
-    
+  const pedido_id = req.body.pedido_id;
+  const customer_id = req.body.customer_id;
+  try {
+    let name = await customersService.getName(customer_id);
+    await smsService.getSmsData(name[0].name, pedido_id);
+    res.send("sucess");
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-
-module.exports = router
+module.exports = router;
